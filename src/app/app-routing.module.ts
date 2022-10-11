0901819f98935c30acfc27494f609952f6577173
DaniAvolio <avoliodaniele9@gmail.com>
2022-10-11 17:04:27 +0200

    fix: added header and implement initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import{HomeComponent} from './home/home.component'
+import{ProdottiComponent} from './prodotti/prodotti.component'
+import{CarrelloComponent} from './carrello/carrello.component'
+import{LogInComponent} from './log-in/log-in.component'
+
+const routes: Routes = [
+{path: '' , component: HomeComponent},
+{path: 'prodotti',component: ProdottiComponent},
+{path: 'carrello',component: CarrelloComponent},
+{path: 'log-in',component: LogInComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProdottiComponent } from './prodotti/prodotti.component';
+import { CarrelloComponent } from './carrello/carrello.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProdottiComponent,
+    CarrelloComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
