8cb3e53595a828951bc43f7dc7c0a46552f9d865
DaniAvolio <avoliodaniele9@gmail.com>
2022-10-14 11:59:44 +0200

    fix: added some elements in the ecommerce

--- src/app/prodotto.service.ts
+++ src/app/prodotto.service.ts
@@ -32,6 +32,18 @@ export class ProdottoService {
     {slug:"prod8", nome:"proteine whey isolate ",
     foto:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/10530911-5884889444360331.jpg",
      prezzo:44.99, marca: "MyProtein", desc:"proteine del siero del latte isolate della myprotein, efficaci e con piu % di proteine, qualita certificata!", categoria:"proteine", feat:"isolate ",feat1:"+85% proteine",feat2:"alta qualità"},
+     {slug:"prod9", nome:"6x barrette proteiche ",
+    foto:"https://static.sscontent.com/thumb/1000/1000/products/124/v1079517_prozis_12-x-protein-snack-30-g_cookies-and-cream_newin_flavor.jpg",
+     prezzo:9.99, marca: "Prozis", desc:"barrette proteiche Prozis, gustose e croccanti. Raggiungi i tuoi obbiettivi consumando queste barrette come snack alternativo", categoria:"barrette", feat:"gustose",feat1:"20% proteine",feat2:"senza lattosio"},
+     {slug:"prod10", nome:"4x barrette proteiche ",
+     foto:"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13308982-3124937164978398.jpg",
+      prezzo:9.99, marca: "MyProtein", desc:"barrette proteiche alta qualità MyProtein, oltre il 40% di proteine e creatina. Disponibili in vari gusti uno più buono dell'altro.", categoria:"barrette", feat:"+40% proteine",feat1:"Alta Qualità",feat2:"con creatina"},
+      {slug:"prod11", nome:"proteine concentrate dell'uovo ",
+      foto:"https://www.bulk.com/media/catalog/product/cache/1500x1500/e/g/egg-white-powder_bpb-ewp-0000_main.jpg",
+       prezzo:24.99, marca: "BulkPowders", desc:"Proteine dell'uovo della bulk, ideale per gli intolleranti al lattosio e per chi vuole uno sprint dalle classiche proteine. Facilmente assorbibili e comunque gustose", categoria:"proteine", feat:"senza lattosio",feat1:"meglio assimilabili",feat2:"70% proteine"},
+       {slug:"prod12", nome:"6x barrette proteiche GyM",
+       foto:"https://static.sscontent.com/thumb/1000/1000/products/124/v1139099_prozis_12-x-protein-snack-35-g_newin.jpg",
+        prezzo:14.99, marca: "Prozis", desc:"barrette proteiche Prozis, gustose e croccanti. con il 60% di proteine queste barrette sono ideali per chi ha un fabbisogno proteico elevata. con 1 ingerisci ben 20 grammi di proteine d ialta qualità!", categoria:"barrette", feat:"con creatina",feat1:"60% proteine",feat2:"alta qualità"}
   ]
 
   private _carrello: Prodotto[] = []
